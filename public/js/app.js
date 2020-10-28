8056b7fd41391306590635f765be7d24ee5d5d56
Shafiq <muhammad.shafiq@itforce-tech.com>
2020-10-28 16:13:01 +0400

    cleaned

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
 
-
-console.log('js file loaded from public folder')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-
-
-
 const weatherForm = document.querySelector('form')
 const m1 = document.querySelector('#p1')
 const m2 = document.querySelector('#p2')
